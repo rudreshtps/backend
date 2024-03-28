b8e9c4540778cfe4d3a75849bc3180f0ad11fc27
rudresh <rudreshtps@gmail.com>
2024-03-28 12:00:27 +0530

    initial

---
+++ exercise/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class QuestionDetails(models.Model):
+    id=models.AutoField(primary_key=True)
+    questionId = models.CharField(max_length=5)
+    studentId = models.CharField(max_length=15)
+    conceptId = models.CharField(max_length=20)
+    currect_ans = models.TextField(null=False)
+    score = models.IntegerField()
+    entered_ans = models.TextField(null=True)
+    skipped_ans = models.TextField(null=True)
+    dateAndTime= models.DateTimeField(auto_now_add=True)
+    session= models.IntegerField()
